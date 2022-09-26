a404bed99c81aba1d21931952de9c7c695fb85fe
ragsav <raghav227996399@gmail.com>
2022-09-27 00:42:11 +0530

    added log

--- js/utils/logger.js
+++ js/utils/logger.js
@@ -1,10 +1,18 @@
 // import Toast from 'react-native-simple-toast';
 
+import {logger} from 'react-native-logs';
+
+var log = logger.createLogger();
 export class Logger {
   static pageLogger = (page, obj) => {
-    // console.log(`-----> : ${page} |=====| `, obj);
+    if (!__DEV__) {
+      return;
+    }
+    const _s = String(page);
+    if (_s.includes('error') || _s.includes('catch')) {
+      log.warn(_s, obj);
+    } else {
+      log.info(_s, obj);
+    }
   };
 }
-export const handleCopyToClipboard = value => {
-  // Toast.show(`Copied to clipboard`);
-};
